6d3cb03f1b012de6a9b786b202659b4163c1078c
Steven M <smartin906@mcad.edu>
2025-02-05 21:05:33 -0600

    exercise 3.1 initial

---
+++ week_03/exercise_3.1/sketch.js
@@ -0,0 +1,26 @@
+let
+
+function setup() {
+  createCanvas(200, 200);
+}
+
+function createTile() {
+  translate(0, 0);
+  stroke('yellow');
+  strokeWeight(2);
+  fill('orange');
+  rect(0, 0, 200, 200);
+  stroke('blue');
+  strokeWeight(10);
+  fill('red');
+  circle(100, 100, 190);
+  circle(100, 100, 150);
+  circle(100, 100, 100);
+  circle(100, 100, 50);
+  line(0, 0, 200, 200);
+  line(200, 0, 0, 200);
+
+}
+function draw () {
+  createTile();
+}
