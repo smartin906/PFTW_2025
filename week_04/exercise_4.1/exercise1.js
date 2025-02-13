9fab2429ab42e4266d94700c74792841f0e02ade
Steven M <smartin906@mcad.edu>
2025-02-13 15:30:34 -0600

    updated to llama verbiage

--- week_04/exercise_4.1/exercise1.js
+++ week_04/exercise_4.1/exercise1.js
@@ -3,7 +3,7 @@ const myEmptyDiv = document.querySelector('#myEmptyDiv');
 //put a h1 element inside of it
 //create a new element
 const heading = document.createElement('h1');
-heading.innerHTML ='hello class! It is a snowy day!';
+heading.innerHTML ='Yes, it is true! Llamas do spit but only do this when they are really upset and would like to warn away others. If you see their ears back and head up, back away!';
 heading.style.cursor = 'pointer';
 heading.addEventListener('click', handleHeadingClick);
 myEmptyDiv.appendChild(heading);
